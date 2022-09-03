f7be4ca430a7d7ed452df77028fc5f91344a412f
Ott Pikk <ott@karmai.eu>
2022-09-03 10:27:24 +0300

    Initialized app2

---
+++ src/app/shared/service/school.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {School} from "../model/school";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SchoolService {
+  private SCHOOL_BASE_URL = 'school';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getAllSchools(): Observable<School[]> {
+    return this.httpClient.get<School[]>(this.SCHOOL_BASE_URL);
+  }
+}
